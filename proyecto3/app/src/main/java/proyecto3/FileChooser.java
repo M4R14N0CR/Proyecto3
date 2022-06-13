e1be9b9fb6afc8c5ba9be6da7fb07a8426882cfb
Brandon1811111 <brandonerv438@gmail.com>
2022-06-13 15:44:24 -0600

    Commit

--- proyecto3/app/src/main/java/proyecto3/FileChooser.java
+++ proyecto3/app/src/main/java/proyecto3/FileChooser.java
@@ -1,21 +1,28 @@
+package proyecto3;
+
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
 public class FileChooser extends JFrame {
-
-	private FileNameExtensionFilter filter = new FileNameExtensionFilter("JPG & PNG Images", "jpg", "png");
-	String selectPath = "";
-
-	public FileChooser(Screen myscreen) {
-		JFileChooser dlg = new JFileChooser();
-		dlg.setFileFilter(filter);
-		int option = dlg.showOpenDialog(dlg);
-		this.selectPath = dlg.getSelectedFile().getPath();
-		myscreen.addImagen(selectPath);
-		myscreen.redimencion(selectPath, myscreen);
-		JOptionPane.showMessageDialog(null, "La imagen ha sido agregada exitosamente");
-	}
-
+  
+  private FileNameExtensionFilter filter = new FileNameExtensionFilter("JPG & PNG Images", "jpg", "png");
+  String selectPath = "";
+  
+  public FileChooser(Screen myscreen) {
+    JFileChooser dlg = new JFileChooser();
+    dlg.setFileFilter(filter);
+    int option = dlg.showOpenDialog(dlg);
+    this.selectPath = dlg.getSelectedFile().getPath();
+    try {
+      myscreen.addImagen(selectPath);
+    } catch (Exception e1) {
+      e1.printStackTrace();
+    }
+    
+    myscreen.redimencion(myscreen);
+    JOptionPane.showMessageDialog(null, "La imagen ha sido agregada exitosamente");
+  }
+  
 }
--- proyecto3/app/src/main/java/proyecto3/Grid.java
+++ proyecto3/app/src/main/java/proyecto3/Grid.java
@@ -1,28 +1,37 @@
-import java.awt.Dimension;
+package proyecto3;
 import javax.swing.BoxLayout;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 
 public class Grid extends JPanel{
-	private Screen myscreen;
-	private keyboard mykey;
-	private JScrollPane scroll;
+    private Screen myscreen;
+    private keyboard mykey;
+    private JScrollPane scroll;
+    
+    public Grid() {
+      myscreen= new Screen();
+      this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+    
+      this.mykey = new keyboard(this);
+      this.mykey.actionButton(this.myscreen);
 
-	public Grid() {
-		this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+      this.scroll = new JScrollPane();
+      
+      this.add(this.mykey);
 
-		this.scroll = new JScrollPane();
+    }  
 
+    public void setScroll() {
+      this.scroll.add(myscreen);
+      this.scroll.setViewportView(myscreen);
+      this.add(this.scroll);
+    }
 
-		this.myscreen = new Screen();
-		this.mykey = new keyboard();
-		this.mykey.actionButton(this.myscreen);
-		this.scroll.add(myscreen);
-
-		this.scroll.setViewportView(myscreen);
-
-		this.add(this.mykey);
-		this.add(this.scroll);
-
-	}
-}
+    public void setScreen(Screen newScreen) {
+      this.myscreen=newScreen;
+    }
+    
+    public Screen getScreen() {
+      return this.myscreen;
+    }
+  }
